ea9b72a1710dc84a150d674d8178c51d408d31a3
styrheim <styrheim@olivant.fo>
2021-06-30 09:53:46 +0100

    Initial commit

---
+++ app/src/main/java/se/miun/jakr1800/dt031g/statechange/MainActivity.java
@@ -0,0 +1,14 @@
+package se.miun.jakr1800.dt031g.statechange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
